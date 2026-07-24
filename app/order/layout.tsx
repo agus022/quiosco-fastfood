9c8e2509e0b722a22e3a87e07c86ccb7e6f1797f
agus022 <flores.jorge.1j@gmail.com>
2026-07-23 22:27:23 -0600

    first commit

---
+++ app/order/layout.tsx
@@ -0,0 +1,12 @@
+import OrderSidebar from "@/components/order/OrderSidebar";
+
+export default function OrderLayout({children,}: Readonly<{children: React.ReactNode;}>) {
+  return (
+    <>
+        <div className="md:flex">
+
+
+        </div>
+    </>
+  );
+}
---
+++ app/products/new/page.tsx
@@ -0,0 +1,5 @@
+export default function CreateProductsPage() {
+  return (
+    <div>CreateProductsPage</div>
+  )
+}
