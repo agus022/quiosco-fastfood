40d6fe1f2a6bc7dc61296362f38e047419014f74
agus022 <flores.jorge.1j@gmail.com>
2026-08-29 14:23:29 -0600

    tableProducts

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -1,12 +1,27 @@
+import ProductTable from '@/components/admin/ProductsTable'
 import Heading from '@/components/ui/Heading'
-import React from 'react'
+import prisma from '@/lib/prisma'
 
-export default function ProductsPage() {
+
+async function getProducts(){
+  const products = await prisma.product.findMany()
+
+  return products
+}
+
+export default async function ProductsPage() {
+
+  const products = await getProducts()
+  //console.log(products)
+
   return (
     <>
         <Heading>
             Administrar productos
         </Heading>
+        <ProductTable
+        products={products}
+        />
     </>
   )
 }
